550f606ccefc419a14c09bc2649f74be336417ba
Chris Suran <suran.c@gmail.com>
2022-04-03 21:42:00 -0400

    Add output for region and tags to use by infra def

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -29,3 +29,11 @@ resource "aws_key_pair" "terraform_example_ec2_key" {
   key_name   = "terraform_example_ec2_key"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC7IGozq19XdGJHdxavVEicuk1Rzwcsl4zaFyJY875jJqh8Lg5P7b0AQxcGFuE8BSDEMb0iPWPn5WeUTuxOdo+hYfKlCCnpcsWIWqm5ULobKvX9zAlkBWsaVAPVVMoxhTyWJON4trUfWrMesOJ8utnohJB/1OevlmKJHdODR3CokZHcXv5uqqvi8wABg8JYOWUis54kPeaEDheaCXH45Gq/8XAav7aAdyK3tU0Zv1YTkrddfSCu2VMNEtXwBD10z59L1FyMBXV/q3o56xMhb6K0eXVrlLOXCg4+PiFVWauJGsjt960EMEcxV6NhnGCjG8xeLLbP/+hLQyBUUAcWN6mI5In9xF0grctKB1gKGoRXpuHTfI+x8I5zDGdSUFfHnt/+ACRczWwl9BmZySNli+Bxzjba8EUyfPkDNIlM/s8oHu3udy4p4notfp4hqLSqnY78DHLLtFX+b8Xvx139iVaXbs7enenwL73QZfIJH7rVTJ8E4kjXLjxPXwRDwKBJs6E= chris@desktop"
 }
+
+output "region" {
+  value = "${var.region}"
+}
+
+output "instanceTags" {
+  value = "${aws_instance.chris_ec2.tags}"
+}
